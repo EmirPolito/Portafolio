def482fd7c1d6489a116ad57cd6f374811838a5d
Emir Polito Guevara <emirpolitog@gmail.com>
2026-05-15 00:47:10 -0600

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -198,7 +198,7 @@ export const DATA = {
 
     {
       title: "RALQ",
-      href: "https://ralq.wasmer.app/",
+      href: "https://ralq-emirpolito.wasmer.app/",
       dates: "Julio 2025",
       active: true,
       description:
@@ -214,7 +214,7 @@ export const DATA = {
       links: [
         {
           type: "Demo",
-          href: "https://ralq.wasmer.app/",
+          href: "https://ralq-emirpolito.wasmer.app/",
           icon: <Icons.globe className="size-3" />,
         },
         {
